87a59ed9c8c30518c0c31b9ed9f1a737398154d8
saifaljuboori <44454675+saifaljuboori@users.noreply.github.com>
2026-08-11 14:54:55 +0300

    Add Docker image build and push functionality

--- vars/buildDockerImage.groovy
+++ vars/buildDockerImage.groovy
@@ -1,2 +1,15 @@
 def call() {
-  
+    echo "building docker image..."
+
+    withCredentials([
+        usernamePassword(
+            credentialsId: 'saifdockerhub',
+            passwordVariable: 'PASSWORD',
+            usernameVariable: 'USERNAME'
+                        )
+    ]) {
+        sh 'docker build -t saljuboori/demo-app:jma-3.0 .'
+        sh 'echo "$PASSWORD" | docker login -u "$USERNAME" --password-stdin'
+        sh 'docker push saljuboori/demo-app:jma-3.0'
+   }
+}
