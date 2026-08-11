89f60988edd0dc7f6b57150fed28b28cc5e05d99
saifaljuboori <44454675+saifaljuboori@users.noreply.github.com>
2026-08-11 17:27:56 +0300

    Enhance Docker image build process with logging
    Added logging for Docker username and Docker info command.

--- vars/buildDockerImage.groovy
+++ vars/buildDockerImage.groovy
@@ -1,15 +1,20 @@
 def call(String imageName) {
     echo "building docker image..."
 
-    withCredentials([
-        usernamePassword(
-            credentialsId: 'saifdockerhub',
-            passwordVariable: 'PASSWORD',
-            usernameVariable: 'USERNAME'
-                        )
-    ]) {
-        sh "docker build -t $imageName ."
-        sh 'echo "$PASSWORD" | docker login -u "$USERNAME" --password-stdin'
-        sh "docker push $imageName"
-   }
+withCredentials([
+    usernamePassword(
+        credentialsId: 'saifdockerhub',
+        passwordVariable: 'PASSWORD',
+        usernameVariable: 'USERNAME'
+    )
+]) {
+    sh "docker build -t $imageName ."
+
+    sh 'echo "Docker username: $USERNAME"'
+
+    sh 'echo "$PASSWORD" | docker login -u "$USERNAME" --password-stdin'
+
+    sh 'docker info 2>/dev/null | grep Username || true'
+
+    sh "docker push $imageName"
 }
