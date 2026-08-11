8dbb919e9c6cf797bad4a1479950a859438c3c52
saifaljuboori <44454675+saifaljuboori@users.noreply.github.com>
2026-08-11 18:00:43 +0300

    Refactor Docker image build process
    Removed unnecessary echo statements and Docker info check.

--- vars/buildDockerImage.groovy
+++ vars/buildDockerImage.groovy
@@ -1,24 +1,15 @@
 def call(String imageName) {
     echo "building docker image..."
-    echo "Image name received: ${imageName}"
 
-withCredentials([
-    usernamePassword(
-        credentialsId: 'saifdockerhub',
-        passwordVariable: 'PASSWORD',
-        usernameVariable: 'USERNAME'
-    )
-]) {
-    sh "docker build -t $imageName ."
-
-    sh 'echo "Docker username: $USERNAME"'
-
-    sh 'echo "$PASSWORD" | docker login -u "$USERNAME" --password-stdin'
-
-    sh 'docker info 2>/dev/null | grep Username || true'
-
-    sh "docker images | grep demo-app"
-
-    sh "docker push $imageName"
-  }
-}    
+    withCredentials([
+        usernamePassword(
+            credentialsId: 'saifdockerhub',
+            passwordVariable: 'PASSWORD',
+            usernameVariable: 'USERNAME'
+        )
+    ]) {
+        sh "docker build -t $imageName ."
+        sh 'echo "$PASSWORD" | docker login -u "$USERNAME" --password-stdin'
+        sh "docker push $imageName"
+    }
+}  
