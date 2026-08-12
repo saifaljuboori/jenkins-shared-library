af8e54cccf8ae2e58af72896739371d29e2fdacc
Saiofy <saif.zidane@outlook.com>
2026-08-12 21:52:28 +0300

    updating builDockerImage var to call from Docker Class

--- vars/buildDockerImage.groovy
+++ vars/buildDockerImage.groovy
@@ -1,15 +1,4 @@
+import com.example.Docker
 def call(String imageName) {
-    echo "building docker image..."
-
-    withCredentials([
-        usernamePassword(
-            credentialsId: 'dockerhub-test',
-            passwordVariable: 'PASSWORD',
-            usernameVariable: 'USERNAME'
-        )
-    ]) {
-        sh "docker build -t $imageName ."
-        sh 'echo "$PASSWORD" | docker login -u "$USERNAME" --password-stdin'
-        sh "docker push $imageName"
-    }
+    return new Docker(this).buildDockerImage(imageName)
 }
