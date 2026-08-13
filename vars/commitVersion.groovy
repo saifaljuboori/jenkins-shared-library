64cc72c73b0b4516187d074835a63586789321d5
saifaljuboori <44454675+saifaljuboori@users.noreply.github.com>
2026-08-14 00:45:37 +0300

    Add commitVersion function for versioning updates

---
+++ vars/commitVersion.groovy
@@ -0,0 +1,21 @@
+def call() {
+    echo "Committing updated application version..."
+
+    withCredentials([
+        usernamePassword(
+            credentialsId: 'github',
+            usernameVariable: 'USERNAME',
+            passwordVariable: 'PASSWORD'
+        )
+    ]) {
+        sh '''
+            git config user.name "Jenkins"
+            git config user.email "jenkins@example.com"
+
+            git add pom.xml
+            git commit -m "chore: increment application version [skip ci]"
+
+            git push https://${USERNAME}:${PASSWORD}@github.com/saifaljuboori/java-maven-app.git HEAD:master
+        '''
+    }
+}
