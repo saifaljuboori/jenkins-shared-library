94666a08a0b4fa5744c99700de6d07c9fa92300a
saifaljuboori <44454675+saifaljuboori@users.noreply.github.com>
2026-08-13 23:04:30 +0300

    Update incrementVersion.groovy

--- vars/incrementVersion.groovy
+++ vars/incrementVersion.groovy
@@ -1,5 +1,5 @@
 def call() {
     echo "Incrementing app version..."
 
-    sh "mvn build-helper:parse-version versions:set '-DnewVersion=\\\${parsedVersion.majorVersion}.\\\${parsedVersion.minorVersion}.\\\${parsedVersion.nextIncrementalVersion}' versions:commit"
+    sh "mvn build-helper:parse-version versions:set '-DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.nextIncrementalVersion}' versions:commit"
 }
