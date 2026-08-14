0b8e5237c86ae584844a1a434d7f1e3fa12ed924
saifaljuboori <44454675+saifaljuboori@users.noreply.github.com>
2026-08-15 02:05:46 +0300

    Refactor docker login credentials handling

--- vars/dockerLoginExercise.groovy
+++ vars/dockerLoginExercise.groovy
@@ -1,5 +1,11 @@
 def call() {
-  withCredentials([usernamePassword(credentialsId: 'saifdockerhub', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
-            sh "echo '${PASSWORD}' | docker login -u '${USERNAME}' --password-stdin"
-  }   
+    withCredentials([
+        usernamePassword(
+            credentialsId: 'saifdockerhub',
+            passwordVariable: 'PASSWORD',
+            usernameVariable: 'USERNAME'
+        )
+    ]) {
+        sh 'echo "$PASSWORD" | docker login -u "$USERNAME" --password-stdin'
+    }
 }  
