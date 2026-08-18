57e8db8b0b88865881e3af145a2e604651c3ddc3
Saiofy <saif.zidane@outlook.com>
2026-08-18 18:30:10 +0300

    updating syntax to incluse stop and remove old version containers

--- vars/ec2Deploy.groovy
+++ vars/ec2Deploy.groovy
@@ -2,8 +2,9 @@ def call() {
     echo "Deploying & running server on EC2..."
 
     def ec2Server = 'ec2-user@3.90.251.184'
-    def dockerCmd = "docker run -d -p 8080:8080 ${env.IMAGE_NAME}"
+    def dockerCmd = "docker stop demo-app || true && docker rm demo-app || true && docker run -d -p 8080:8080 ${env.IMAGE_NAME}"
+
     sshagent(credentials: ['ec2-server-key']) {
-        sh "ssh -o StrictHostKeyChecking=no ${ec2Server} ${dockerCmd}"
+        sh "ssh -o StrictHostKeyChecking=no ${ec2Server} '${dockerCmd}'"
     }
 }
